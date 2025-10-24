042e645bed4d1b593f78aaacce871b1f7cc38750
oono <wahull524@yahoo.co.jp>
2025-10-24 13:25:05 +0900

    表を作ってみました

--- src/app/analysis/components/whole.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { useLanguage } from "@/contexts/LanguageContext";
-import { PlayerData, Player } from "../types";
